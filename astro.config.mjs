75c0c9d1ef4ff117a2f41ae56f8537143757b445
Henry Percy <henry@rework.digital>
2024-01-07 12:17:13 +0000

    feat: add Expressive Code package for improved syntax highlighting in MDX files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
+import astroExpressiveCode from 'astro-expressive-code'
 import netlify from "@astrojs/netlify/functions";
+
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+const astroExpressiveCodeOptions = {
+  themes: ['one-dark-pro', 'github-light'],
+  shiki: {
+
+  },
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), astroExpressiveCode(astroExpressiveCodeOptions), mdx()]
   // output: "server",
   // adapter: netlify()
 });
